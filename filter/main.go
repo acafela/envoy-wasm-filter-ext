8181d98d42580c0917c800d69387bd7d5b2497c7
acafela <acafela91@gmail.com>
2022-05-18 18:24:07 +0900

    프로젝트 생성

---
+++ filter/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"github.com/tetratelabs/proxy-wasm-go-sdk/proxywasm"
+	"github.com/tetratelabs/proxy-wasm-go-sdk/proxywasm/types"
+)
+
+func main() {
+	proxywasm.SetVMContext(&vmContext{})
+}
+
+type vmContext struct {
+	// Embed the default VM context here,
+	// so that we don't need to reimplement all the methods.
+	types.DefaultVMContext
+}
+
+// Override types.DefaultVMContext.
+func (*vmContext) NewPluginContext(contextID uint32) types.PluginContext {
+	return &pluginContext{}
+}
+
+type pluginContext struct {
+	// Embed the default plugin context here,
+	// so that we don't need to reimplement all the methods.
+	types.DefaultPluginContext
+}
+
+// Override types.DefaultPluginContext.
+func (*pluginContext) NewHttpContext(contextID uint32) types.HttpContext {
+	return &httpHeaders{contextID: contextID}
+}
+
+type httpHeaders struct {
+	// Embed the default http context here,
+	// so that we don't need to reimplement all the methods.
+	types.DefaultHttpContext
+	contextID uint32
+}
+
+// Override types.DefaultHttpContext.
+func (ctx *httpHeaders) OnHttpResponseHeaders(numHeaders int, endOfStream bool) types.Action {
+	addErr := proxywasm.AddHttpResponseHeader("x-test", "hello, wasm filter!")
+	if addErr != nil {
+		proxywasm.LogCriticalf("failed to get response headers: %v", addErr)
+	}
+
+	return types.ActionContinue
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module envoy-wasm-filter-ext
+
+go 1.18
+
+require github.com/tetratelabs/proxy-wasm-go-sdk v0.18.0
